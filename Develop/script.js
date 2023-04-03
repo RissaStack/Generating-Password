2e47b5e5e3d958cc85683f0ce8707b8cf97f9560
Larissa Stack <larissastack@gmail.com>
2023-04-03 09:21:39 -0700

    added comments and updated README.

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// // appropriate characters for a password
+// // recognized characters for a password
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var number = "0123456789";
 var special = " ~`-_=+!@#$%^&*()";
 
 
-// Write password to the #password input
+// This function is to write the password using the desired characters.
 function writePassword () {
   var password = generatePassword();
 
-
+//This loop is used to continue to give the prompt to select at least one char set if a user does not select a char set.
   while (password === null) {
     password = generatePassword();
   };
@@ -23,16 +23,16 @@ function writePassword () {
 
 
 function generatePassword () {
-  var selected = "";
-  var password = "";
+  var selected = ""; //This var shows what character sets were selected.
+  var password = ""; //This var is the password using those character sets.
 
-
+//These vars are what is shown in the prompt. 
   var lowerCaseChoice = confirm("Do you want to include lowercase letters?");
   var upperCaseChoice = confirm("Do you want to include uppercase letters?");
   var numChoice = confirm("Do you want to include numbers?");
   var specialChoice = confirm("Do you want to include special characters?");
 
-
+//These conditional statements show that an alert will be put on the screen to select one char type if the user does not select any.
   if (
       !lowerCaseChoice &&
       !upperCaseChoice &&
@@ -43,10 +43,10 @@ function generatePassword () {
       return null;
   }
 
-
+//This var gives the user a prompt to ask how many characters they will like in their password.   
   var lengthChoice = Number(prompt("How long would you like your password to be - must be between 8 - 128 characters."));
 
-
+//This loop is used to check that the length the user chooses is between 8 and 128. If it isn't, it will give the following alert. 
   while (
     !lengthChoice ||
     !(lengthChoice >= 8) ||
@@ -56,7 +56,7 @@ function generatePassword () {
     lengthChoice = Number(prompt("How long would you like your password to be - must be between 8 - 128 characters."));
   }
 
-
+//This conditional statement figures out what charcters the user wants and puts them into the var selected. 
   if (lowerCaseChoice) {
     selected += lowerCase
   };
@@ -70,13 +70,13 @@ function generatePassword () {
     selected += special
   };
 
-
+// This loop is working with the var selected to add one character at a time to the password until it reaches the legthChoice that the user selected. The Math.floor and Math.raondom randomly select the number and round down to select the character. 
   for (let i = 0; i < lengthChoice; i++) {
     var index = Math.floor(Math.random() * selected.length);
     password += selected[index];
   };
 
-
+//This is where we get our password returned to the user. 
   return password;
 };
 
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Generating-Password
+
+## Decription
+
+This project was done to add Javascript to the given code. Adding Javascript allowed the password generator to function properly using loops, conditional statements, math equations, and alerts.  
+
+## Challenges
+
+Some of the challenges I faced were getting the length of the password to be recognized. I used Google and Dev to figure out that i needed to use a while loop and space each conditional statment out to it's own line in order to work. I still need to work with loops to fully understand the difference between a for loop and a while loop. 
+
+## Created With
+
+HTML, CSS, and Javascript
+
+## Deployed Image
+
+![Image of application](/assets/03-javascript-homework-demo.png  "Password Generator")
